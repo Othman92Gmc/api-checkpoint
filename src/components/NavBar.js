c19f5121fd61225745985495279eaa318078c425
Othman92GMC <othman.stambouli@gmail.com>
2022-02-15 04:13:08 +0100

    API-checkpoint

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import UserList from './UserList'
+
+const NavBar = () => {
+    return (
+    <div className="flex">
+    <ul className="menu-bar">
+    <h1>Members: {' '}</h1>
+    <hr></hr>
+
+</ul>
+    </div>
+    )
+}
+
+export default NavBar
